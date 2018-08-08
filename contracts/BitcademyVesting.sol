c2e8ee7ae5626e167c22aaf4d36a9e77fc7b9908
Kelvin Ndereba <nderebakelvin@gmail.com>
2018-08-08 12:46:42 +0800

    Update on Timelock Logic
    Added release intervals

--- contracts/BitcademyVesting.sol
+++ contracts/BitcademyVesting.sol
@@ -18,11 +18,17 @@ contract BitcademyVesting is Ownable {
   uint256 public cliff;
   uint256 public start;
   uint256 public duration;
+  uint256 public interval;
+
+  uint256 public constant ReleaseCap = 150000000000000000000000000;
 
   mapping (address=>bool) public members;
+  mapping (address =>uint) public numReleases;
+  mapping (address => uint) public nextRelease;
   uint256 public noOfMembers;
   uint256 public released;
-
+  //uint256 public standardQuantity;
+  uint public constant Releases = 18;
   /**
    * @dev Creates a vesting contract that vests its balance of any BitcademyToken token to the
    * _beneficiary, gradually in a linear fashion until _start + _duration. By then all
@@ -34,7 +40,8 @@ contract BitcademyVesting is Ownable {
   constructor(
     uint256 _start,
     uint256 _cliff,
-    uint256 _duration
+    uint256 _duration,
+    uint256 _interval
   )
     public
   {
@@ -43,6 +50,7 @@ contract BitcademyVesting is Ownable {
     duration = _duration;
     cliff = _start.add(_cliff);
     start = _start;
+    interval = _interval;
   }
 
   modifier onlyMember(address _memberAddress) {
@@ -51,11 +59,13 @@ contract BitcademyVesting is Ownable {
   }
 
   function addMember(address _member) public onlyOwner {
+      require(members[_member] == false);
       members[_member] = true;
       noOfMembers = noOfMembers.add(1);
   }
 
   function removeMember(address _member) public onlyOwner {
+      require(members[_member] == true);
       members[_member] = false;
       noOfMembers = noOfMembers.sub(1);
   }
@@ -69,11 +79,19 @@ contract BitcademyVesting is Ownable {
 
     require(unreleased > 0);
 
+    require(numReleases[_member] <= Releases);
+    if (numReleases[_member] == 0){
+     nextRelease[_member] = interval.add(cliff);
+   }
+    require(nextRelease[_member] >= block.timestamp);
     released = released.add(unreleased);
-
-    unreleased = unreleased.div(noOfMembers);
+    unreleased = ReleaseCap.div(noOfMembers.mul(Releases));
+    //unreleased =  unreleased.div(noOfMembers);
 
     _token.transfer(_member, unreleased);
+
+    numReleases[_member]++;
+    nextRelease[_member] = nextRelease[_member].add(interval);
 
     emit Released(unreleased);
   }
