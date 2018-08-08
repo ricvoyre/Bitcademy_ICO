efd214d32e48160d6aaa922d76c574dfb0fa6cfe
Kelvin Ndereba <nderebakelvin@gmail.com>
2018-08-08 12:48:25 +0800

    changes on BTMGVesting contract
    Added interval and release limits

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -6,7 +6,7 @@ var BitcademyVesting = artifacts.require("./BitcademyVesting.sol");
 
 
 module.exports = function(deployer) {
-    deployer.deploy(BitcademyVesting, 1533661200,600,10800);
-    //deployer.deploy(BitcademyToken,"0x7e60b69435E6408E92EA2FBf5A047495911c6012","0x493f54c10679ad3b5fb307ba68eb69280db471db",1000000000);
+    deployer.deploy(BitcademyVesting,1533702180,600,10800,600);
+    //deployer.deploy(BitcademyToken,"0x7e60b69435E6408E92EA2FBf5A047495911c6012","0xa85799bf24de3c0b93335fbc8ccf8c973c757e7d",1000000000);
   //deployer.deploy(PreICOBitAcademyGold,73079733835916,"0x37f90f9BE74C6Af83e2eFA6A918ca5D38eB655e4","0x6454835367dfe5bcb3787fdb968ac0bdc3ece1d9",1529487347,1529573747, "0x37f90f9BE74C6Af83e2eFA6A918ca5D38eB655e4");
 };
